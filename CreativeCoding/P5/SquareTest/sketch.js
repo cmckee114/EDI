aac702a6c0dc8d13bbe64eed281a45caf3b8759d
cmckee114 <cassidyrosemckee@gmail.com>
2021-11-23 08:03:27 -0500

    p5js

---
+++ CreativeCoding/P5/SquareTest/sketch.js
@@ -0,0 +1,16 @@
+function setup() {//runs once
+  createCanvas(720, 720);
+  fill('red');
+  strokeWeight(8);
+}
+
+function draw() {//runs in a loop
+  background(220);
+  
+  translate(60,60);
+  
+  quad(0,0,
+       300,0,
+       300,300,
+       0,300);
+}
