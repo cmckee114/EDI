f3fc1b9855b8dba934d924032695d22fbfcc8f90
cmckee114 <cassidyrosemckee@gmail.com>
2021-12-07 07:48:28 -0500

    done

--- README.md
+++
@@ -1,2 +0,0 @@
-# EDI This is my Github!
-https://cmckee114.github.io/EDI/
---
+++ sketch.js
@@ -0,0 +1,38 @@
+let gif1;
+let gif3;
+
+function preload (){
+  gif1 = loadImage ('assets/gif1test2.gif');
+  gif3 = loadImage ('assets/gif3.gif');
+}
+
+function setup() { //runs once
+  frameRate(1);
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+
+background (255);
+
+var num = 20; //variable for the number of squares in the array
+var sideLen = windowWidth/num;//variable for the side lenth of each square
+  for (var y = 0; y < windowHeight; y = y + sideLen) {//loop to create rows in the y direction
+     for (var x = 0; x < windowWidth; x = x + sideLen) {// loop to create a row of squares in the x direction0
+
+       var r = floor(random(2));
+       switch (r) {
+            case 0:
+              image(gif1,x,y)
+            case 1:
+              image(gif3,x,y)
+      }
+}
+}
+//noLoop ();
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
