ac502108b34238e0d7f3c14457ad2022ecc219c6
cmckee114 <cassidyrosemckee@gmail.com>
2022-01-13 12:26:14 -0500

    here

--- CreativeCoding/MachDraw/machineDrawing/sketch.js
+++ CreativeCoding/MachDraw/machineDrawing/sketch.js
@@ -4,9 +4,11 @@ need to have p5.svg.js in project and in index.html
 see -https://github.com/zenozeng/p5.js-svg
 this will save an SVG file in your download folder
 */
+var pW = 11
+var pH = 8.5
 
 function setup() {
-  createCanvas(1000, 500, SVG); // Create SVG Canvas
+  createCanvas(pW*72, pH*72, SVG); // Create SVG Canvas
   strokeWeight(1); // do 0.1 for laser
   stroke(0, 0, 0); // red is good for laser
   noFill(); // better not to have a fill for laser
@@ -15,7 +17,7 @@ function setup() {
 function draw() {
 
   for(let x=0; x<250; x+=6) {
-   line(x, 0, x+120, 500)
+   line(x, 0, x+120, 700)
   }
   for(let i = 0; i <= 500; i+=6){
     line(250, i-200, 500, i+150)
@@ -27,9 +29,9 @@ function draw() {
     line(750, 250+i, 600+i, 500)
   }
   for (let i=0; i<=250; i+=5) {
-    line(750, i, 1000-i, 250)
-    line(1000-i, 0, 1000, i)
-    line(750, 450+i, 1000-i, 500)
+    line(900, i, 1000-i, 250)
+    line(1200-i, 0, 900, i)
+    line(500, 450+i, 1000-i, 620)
     line(1000, 250+i, 1000-i, 250)
   }
 
